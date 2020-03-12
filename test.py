34d2a175a99465d265341a79b4624ab436c05ee4
vietle94 <viet.letranhoang@gmail.com>
2020-03-12 20:34:43 +0200

    declare folder paths, save csv for snr, depo save img

--- test.py
+++ test.py
@@ -4,12 +4,22 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import halo_data as hd
+from pathlib import Path
+import csv
 %matplotlib qt
+
+# %%
+data_folder = r'F:\halo\32\depolarization'
+image_folder = r'F:\halo\32\depolarization\img'
+depo_folder = r'F:\halo\32\depolarization\depo'
+snr_folder = r'F:\halo\32\depolarization\snr'
+
+for path in [image_folder, depo_folder, snr_folder]:
+    Path(path).mkdir(parents=True, exist_ok=True)
+
 # %%
 # load whole folder data
-data = hd.getdata("C:/Users/LV/OneDrive - University of Helsinki/FMI/halo/53/depolarization/")
-# data = hd.getdata("G:/OneDrive - University of Helsinki/FMI/halo/53/depolarization/")
-# data = hd.getdata(r'G:\OneDrive - University of Helsinki\FMI\halo\53\depolarization')
+data = hd.getdata(data_folder)
 
 # %% get data
 # pick day of data
@@ -35,20 +45,21 @@ df.meta_data('co_signal')
 
 # %%
 # Change masking missing values from -999 to NaN
-# df.unmask999()
+df.unmask999()
 # Remove first three columns of data matrix due to calibration,
 # they correspond to top 3 height data
 # ask Ville for more detail
-df.filter_height()
+# df.filter_height()
 # Overview of data
 df.describe()
 
 # %%
 # Plot data
 
-df.plot(
+image_raw = df.plot(
     variables=['beta_raw', 'v_raw', 'cross_signal', 'depo_raw', 'co_signal',
                'cross_signal_averaged', 'depo_averaged_raw', 'co_signal_averaged'], ncol=2, size=(20, 15))
+image_raw.savefig(image_folder + '/' + df.filename + '_raw.png')
 
 # %% Histogram of an area in SNR plot
 
@@ -67,12 +78,9 @@ fig.colorbar(p, ax=ax[0])
 # %% Calculate threshold
 noise = area.area - 1
 threshold = 1 + np.nanstd(noise) * 3
-
-np.nanstd(noise)
 threshold
 
 # %% Histogram of an area in SNR plot
-
 fig, ax = plt.subplots(1, 2, figsize=(24, 12))
 p = ax[0].pcolormesh(df.data['time_averaged'],
                      df.data['range'],
@@ -91,6 +99,18 @@ threshold_averaged = 1 + np.nanstd(noise_averaged) * 2
 
 threshold_averaged
 
+# %%
+# Append to or create new csv file
+with open(snr_folder + '/' + df.filename + '.csv', 'a') as filedata:
+    writer = csv.writer(filedata)
+    writer.writerow([df.more_info['year'],
+                     df.more_info['month'],
+                     df.more_info['day'],
+                     df.more_info['location'].decode('utf-8'),
+                     df.more_info['systemID'],
+                     threshold,
+                     threshold_averaged])
+
 
 # %%
 df.filter(variables=['beta_raw', 'v_raw', 'cross_signal', 'depo_raw'],
@@ -102,19 +122,16 @@ df.filter(variables=['cross_signal_averaged', 'depo_averaged_raw'],
 # %%
 # Plot data
 
-df.plot(
+image_filtered = df.plot(
     variables=['beta_raw', 'v_raw', 'cross_signal', 'depo_raw', 'co_signal',
                'cross_signal_averaged', 'depo_averaged_raw', 'co_signal_averaged'], ncol=2, size=(20, 15))
 
-
+image_filtered.savefig(image_folder + '/' + df.filename + '_filtered.png')
 # %% Summary
 df.describe()
 
 # %%
-
-final_result = pd.DataFrame(columns=['time', 'range', 'SNR', 'depo'])
-# %%
-
+# Area selection for depo cloud
 fig = plt.figure(figsize=(24, 12))
 ax1 = fig.add_subplot(211)
 ax2 = fig.add_subplot(223)
@@ -125,12 +142,10 @@ p = ax1.pcolormesh(df.data['time'],
                    cmap='jet', vmin=df.cbar_lim['beta_raw'][0],
                    vmax=df.cbar_lim['beta_raw'][1])
 me = fig.colorbar(p, ax=ax1)
-ax1.set_title(df.full_data.filename.split('\\')[-1].split('_')[0] + ' - ' +
-              df.more_info['location'].decode("utf-8") + ' - ' +
-              str(df.more_info['systemID']),
-              size=30,
-              weight='bold')
-
+ax1.set_title('depo')
+fig.suptitle(df.filename,
+             size=30,
+             weight='bold')
 area = hd.area_select(df.data['time'],
                       df.data['range'],
                       df.data['depo_raw'].transpose(),
@@ -140,32 +155,31 @@ area = hd.area_select(df.data['time'],
                       type='time_point',
                       ref=df.data['co_signal'])
 
-final_result = pd.DataFrame(columns=['time', 'range', 'SNR', 'depo'])
-
 # %%
 i = area.i
 area_value = area.area[:, i]
 area_range = df.data['range'][area.maskrange]
 area_snr = df.data['co_signal'].transpose()[area.mask][:, i]
-
+area_vraw = df.data['v_raw'].transpose()[area.mask][:, i]
+area_betaraw = df.data['beta_raw'].transpose()[area.mask][:, i]
+area_cross = df.data['cross_signal'].transpose()[area.mask][:, i]
+# Calculate indice of maximum snr value
 max_i = np.argmax(area_snr)
-result = pd.Series({'time': df.data['time'][area.masktime][i],
-                    'range': area_range[max_i],
-                    'SNR': area_snr[max_i],
-                    'depo': area_value[max_i]})
-result
-final_result = final_result.append(result, ignore_index=True)
-final_result
 
-# %%
-plt.plot(final_result['depo'], '-')
-
-# %%
-# Location etc
-name = [int(df.more_info.get(key)) if key != 'location' else
-        df.more_info.get(key).decode("utf-8") for
-        key in ['location', 'year', 'month', 'day', 'systemID']]
-
-filename = '-'.join([str(elem) for elem in name])
-
-final_result.to_csv(filename + '.csv')
+result = [df.more_info['year'],
+          df.more_info['month'],
+          df.more_info['day'],
+          df.more_info['location'].decode('utf-8'),
+          df.more_info['systemID'],
+          df.data['time'][area.masktime][i],  # time as hour
+          area_range[max_i],  # range
+          area_value[max_i],  # depo value
+          area_snr[max_i],  # snr
+          area_vraw[max_i],  # v_raw
+          area_betaraw[max_i],  # beta_raw
+          area_cross[max_i]  # cross_signal
+          ]
+# Append to or create new csv file
+with open(depo_folder + '/' + df.filename + '.csv', 'a') as filedata:
+    writer = csv.writer(filedata)
+    writer.writerow(result)
