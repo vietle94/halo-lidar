46c4df73ef086af34c54a753798587ee1a0aeb9d
vietle <viet.letranhoang@gmail.com>
2020-09-14 16:29:44 +0300

    inspect for averaged data

--- inspect_data.py
+++ inspect_data.py
@@ -16,8 +16,31 @@ df.filter_height()
 df.unmask999()
 df.depo_cross_adj()
 
-df.filter(variables=['beta_raw'],
-          ref='co_signal',
-          threshold=1 + 3*df.snr_sd)
+# %%
+# Modify t: time interval
+t = 1
+n = 1
+while True:
+    dif = np.median(df.data['time'][n::n] - df.data['time'][:-n:n])
+    if t - dif < 0:
+        break
+    n += 1
 
-df.inspect_data()
+# %%
+df.average(n)
+df.data['depo_averaged'] = hd.ma(df.data['depo_averaged'].T, n=5).T
+df.filter(variables=['depo_averaged'],
+          ref='co_signal_averaged',
+          threshold=1 + 3*df.snr_sd/np.sqrt(n))
+
+
+# %%
+fig, ax = plt.subplots()
+ax.pcolormesh(df.data['time_averaged'], df.data['range'],
+              df.data['depo_averaged'].T, cmap='jet', vmin=0, vmax=0.5)
+p = hd.area_select(df.data['time_averaged'], df.data['range'],
+                   df.data['depo_averaged'].T, ax_in=ax, fig=fig)
+
+# %%
+p.area
+np.nanmean(p.area)
