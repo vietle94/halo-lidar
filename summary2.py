9005713d1b6ff51a501a49ce29f5b2561ebfad92
vietle <viet.letranhoang@gmail.com>
2020-09-28 12:46:21 +0300

    added white color for missing data

--- summary2.py
+++ summary2.py
@@ -3,9 +3,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.stats import binned_statistic_2d
 from matplotlib.colors import LogNorm
+import matplotlib
 %matplotlib qt
 
 # %%
+my_cmap = matplotlib.cm.get_cmap('jet')
+my_cmap.set_under('w')
+bin_depo = np.linspace(0, 0.5, 50)
+bin_month = np.arange(0.5, 13, 1)
+bin_time = np.arange(0, 25)
+
 for site in ['46', '54', '33', '53', '34', '32']:
     save_location = 'F:\\halo\\classifier\\summary\\'
     df = pd.read_csv('F:\\halo\\classifier\\' + site + '\\result.csv')
@@ -14,9 +21,6 @@ for site in ['46', '54', '33', '53', '34', '32']:
     df['year'] = df['date'].dt.year
     df['month'] = df['date'].dt.month
     df['depo'][df['depo'] > 1] = np.nan
-    bin_depo = np.linspace(0, 0.5, 50)
-    bin_month = np.arange(0.5, 13, 1)
-    bin_time = np.arange(0, 25)
 
     for key, grp in df.groupby('year'):
         title = str(key) + '_' + df['location'][0]
@@ -27,7 +31,7 @@ for site in ['46', '54', '33', '53', '34', '32']:
         X, Y = np.meshgrid(month_edge, depo_edge)
 
         fig, ax = plt.subplots(figsize=(12, 6))
-        p = ax.pcolormesh(X, Y, H.T, cmap='jet')
+        p = ax.pcolormesh(X, Y, H.T, cmap=my_cmap, vmin=0.001)
         fig.colorbar(p, ax=ax)
         ax.set_xlabel('Month')
         ax.set_ylabel('Depolarization ratio')
@@ -44,7 +48,7 @@ for site in ['46', '54', '33', '53', '34', '32']:
         X, Y = np.meshgrid(time_edge, month_edge)
 
         fig, ax = plt.subplots(figsize=(12, 6))
-        p = ax.pcolormesh(X, Y, dep_mean.T, cmap='jet', vmin=0, vmax=0.3)
+        p = ax.pcolormesh(X, Y, dep_mean.T, cmap=my_cmap, vmin=0.001, vmax=0.3)
         ax.set_xlabel('Time')
         ax.set_ylabel('Month')
         cbar = fig.colorbar(p, ax=ax)
