4191be19b865edd9bea35d316f26caf2d2b3ec58
vietle <viet.letranhoang@gmail.com>
2020-10-06 20:47:45 +0300

    change inspect_data0 to basic

--- inspect_data0.py
+++ inspect_data0.py
@@ -8,57 +8,27 @@ import glob
 data = hd.getdata('F:/halo/33/depolarization')
 
 # %%
-date = '20160508'
+date = '20160314'
 file = [file for file in data if date in file][0]
 df = hd.halo_data(file)
 
 df.filter_height()
 df.unmask999()
-# df.depo_cross_adj()
-
-# # %%
-# # Modify t: time interval
-# t = 1
-# n = 1
-# while True:
-#     dif = np.median(df.data['time'][n::n] - df.data['time'][:-n:n])
-#     if t - dif < 0:
-#         break
-#     n += 1
+df.depo_cross_adj()
 
 # %%
-##############################
-# Choose time interval unit is hour
-##############################
-t = (df.data['time'] > 20) & (df.data['time'] < 24)
-
-co = df.data['co_signal'][t, :] - 1
-cross = df.data['cross_signal'][t, :] - 1
-cro_sd = np.nanstd(cross, axis=0)
+fig, ax = plt.subplots()
+ax.pcolormesh(df.data['time'], df.data['range'],
+              np.log10(df.data['beta_raw']).T, cmap='jet', vmin=-8, vmax=-4)
+p = hd.area_select(df.data['time'], df.data['range'],
+                   df.data['depo_raw'].T, ax_in=ax, fig=fig)
 
 # %%
-fig, ax = plt.subplots(1, 2, sharey=True, figsize=(12, 6))
-ax[0].plot(np.nanmean(co, axis=0), df.data['range'], label='co_SNR')
-ax[0].plot(np.nanmean(cross, axis=0), df.data['range'], label='cross_SNR')
-ax[0].errorbar(np.nanmean(cross, axis=0)[::10], df.data['range'][::10],
-               xerr=cro_sd[::10])
-ax[0].legend()
-ax[0].axvline(x=0)
-ax[0].set_title('Mean cross_signal profile with green errorbars')
+df.filter(variables=['depo_adj', 'depo_adj_sd'],
+          ref='co_signal',
+          threshold=1 + 3*df.snr_sd)
 
-ax[1].plot(np.nanmean(co, axis=0), df.data['range'], label='co_SNR')
-ax[1].plot(np.nanmean(cross, axis=0), df.data['range'], label='cross_SNR')
-ax[1].legend()
-ax[1].axvline(x=0)
-ax[1].set_xlim([- 0.0005, 0.002])
-ax[1].set_title('Zoomed image')
-fig.tight_layout()
-# fig.savefig('no_bleedthrough.png', bbox_inches='tight')
+dep = np.nanmean(df.data['depo_adj'].T[p.mask])
+dep_sd = np.nanmean(df.data['depo_adj_sd'].T[p.mask])
 
-# %%
-fig, ax = plt.subplots(figsize=(12, 6))
-p = ax.pcolormesh(df.data['time'], df.data['range'],
-                  np.log10(df.data['beta_raw']).T, vmin=-8, vmax=-4, cmap='jet')
-fig.colorbar(p, ax=ax)
-ax.set_title('beta_raw')
-# fig.savefig('beta.png', bbox_inches='tight')
+print(dep, dep_sd)
