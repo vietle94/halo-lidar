ae7412ca65aad4503d05e042402f6fa9b31a7d21
vietle <viet.letranhoang@gmail.com>
2020-11-12 12:44:17 +0200

    remove individual plots

--- summary2.py
+++ summary2.py
@@ -41,53 +41,6 @@ bin_month = np.arange(0.5, 13, 1)
 bin_time = np.arange(0, 25)
 
 # %%
-save_location = 'F:\\halo\\classifier\\summary\\'
-
-for site in ['46', '54', '33', '53', '34', '32']:
-    df = pd.read_csv('F:\\halo\\classifier\\' + site + '\\result.csv')
-
-    df['date'] = pd.to_datetime(df['date'])
-    df['year'] = df['date'].dt.year
-    df['month'] = df['date'].dt.month
-    df['depo'][df['depo'] > 1] = np.nan
-
-    for key, grp in df.groupby('year'):
-        title = str(key) + '_' + df['location'][0]
-        H, month_edge, depo_edge = np.histogram2d(
-            grp['month'], grp['depo'],
-            bins=(bin_month, bin_depo)
-        )
-        X, Y = np.meshgrid(month_edge, depo_edge)
-
-        fig, ax = plt.subplots(figsize=(12, 6))
-        p = ax.pcolormesh(X, Y, H.T, cmap=my_cmap, vmin=0.001)
-        fig.colorbar(p, ax=ax)
-        ax.set_xlabel('Month')
-        ax.set_ylabel('Depolarization ratio')
-        ax.set_title(title, size=22, weight='bold')
-        fig.savefig(save_location + title + '_month_depo.png', bbox_inches='tight')
-
-        dep_mean, time_edge, month_edge, _ = binned_statistic_2d(
-            grp['time'],
-            grp['month'],
-            grp['depo'],
-            bins=[bin_time, bin_month],
-            statistic=np.nanmean)
-
-        X, Y = np.meshgrid(time_edge, month_edge)
-
-        fig, ax = plt.subplots(figsize=(12, 6))
-        p = ax.pcolormesh(X, Y, dep_mean.T, cmap=my_cmap, vmin=0.001, vmax=0.3)
-        ax.set_xlabel('Time')
-        ax.set_ylabel('Month')
-        cbar = fig.colorbar(p, ax=ax)
-        cbar.ax.set_ylabel('Depolarization ratio')
-        cbar.ax.yaxis.set_label_position('left')
-        ax.set_title(title, size=22, weight='bold')
-        fig.savefig(save_location + title + '_month_hour.png', bbox_inches='tight')
-        plt.close('all')
-
-# %%
 df = pd.DataFrame()
 for site in ['46', '54', '33', '53', '32']:
     save_location = 'F:\\halo\\classifier2\\summary\\'
@@ -160,7 +113,7 @@ for key, val in avg.items():
     axes[pos[k], -1].xaxis.set_ticks([4, 8, 12])
 axes[0, -1].set_title('4 years averaged', weight='bold', size=15)
 fig.tight_layout()
-# fig.savefig(save_location + 'month_depo.png', bbox_inches='tight')
+fig.savefig(save_location + 'month_depo.png', bbox_inches='tight')
 
 # %%
 avg = {}
