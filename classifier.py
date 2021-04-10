52f556e98b9d715c3fc68b9be7e9448bb40f10a6
vietle <viet.letranhoang@gmail.com>
2021-04-11 00:57:46 +0300

    remove the elevated aerosol class, remove save as csv

--- classifier.py
+++ classifier.py
@@ -33,12 +33,6 @@ df.filter(variables=['beta_raw'],
           ref='co_signal',
           threshold=1 + df.snr_sd)
 
-# Save before further filtering
-beta_save = df.data['beta_raw'].flatten()
-depo_save = df.data['depo_adj'].flatten()
-co_save = df.data['co_signal'].flatten()
-cross_save = df.data['cross_signal'].flatten()  # Already adjusted with bleed
-
 df.data['classifier'] = np.zeros(df.data['beta_raw'].shape, dtype=int)
 
 log_beta = np.log10(df.data['beta_raw'])
@@ -159,7 +153,7 @@ if (df.data['classifier'] == 10).any():
         elif r_dict[key] == min(df.data['range']):
             lab[db.labels_ == key] = 10
         elif (val > -0.2):
-            lab[db.labels_ == key] = 11
+            lab[db.labels_ == key] = 10
         else:
             lab[db.labels_ == key] = 40
 
@@ -196,65 +190,41 @@ if (df.data['classifier'] == 20).any():
 
 # %%
 cmap = mpl.colors.ListedColormap(
-    ['white', '#2ca02c', '#808000', 'blue', 'red', 'gray'])
-boundaries = [0, 10, 11, 20, 30, 40, 50]
+    ['white', '#2ca02c', 'blue', 'red', 'gray'])
+boundaries = [0, 10, 20, 30, 40, 50]
 norm = mpl.colors.BoundaryNorm(boundaries, cmap.N, clip=True)
 
-# %%
-fig, axes = plt.subplots(6, 2, sharex=True, sharey=True,
-                         figsize=(16, 9))
-for val, ax, cmap_ in zip([aerosol, aerosol_smoothed,
-                           liquid_smoothed, precipitation_1_median,
-                           updraft_median,
-                           precipitation_1_median_smooth, precipitation_1_low,
-                           updraft_ebola_max, precipitation],
-                          axes.flatten()[2:-1],
-                          [['white', '#2ca02c'], ['white', '#2ca02c'],
-                           ['white', 'red'], ['white', 'blue'],
-                           ['white', '#D2691E'],
-                           ['white', 'blue'], ['white', 'blue'],
-                           ['white', '#D2691E'], ['white', 'blue']]):
-    ax.pcolormesh(df.data['time'], df.data['range'],
-                  val.T, cmap=mpl.colors.ListedColormap(cmap_))
-axes.flatten()[-1].pcolormesh(df.data['time'], df.data['range'],
-                              df.data['classifier'].T,
-                              cmap=cmap, norm=norm)
-axes[0, 0].pcolormesh(df.data['time'], df.data['range'],
-                      np.log10(df.data['beta_raw']).T,
-                      cmap='jet', vmin=-8, vmax=-4)
-axes[0, 1].pcolormesh(df.data['time'], df.data['range'],
-                      df.data['v_raw'].T, cmap='jet', vmin=-2, vmax=2)
-fig.tight_layout()
-fig.savefig(classifier_folder + '/' + df.filename + '_classifier.png',
-            dpi=150, bbox_inches='tight')
+# # %%
+# fig, axes = plt.subplots(6, 2, sharex=True, sharey=True,
+#                          figsize=(16, 9))
+# for val, ax, cmap_ in zip([aerosol, aerosol_smoothed,
+#                            liquid_smoothed, precipitation_1_median,
+#                            updraft_median,
+#                            precipitation_1_median_smooth, precipitation_1_low,
+#                            updraft_ebola_max, precipitation],
+#                           axes.flatten()[2:-1],
+#                           [['white', '#2ca02c'], ['white', '#2ca02c'],
+#                            ['white', 'red'], ['white', 'blue'],
+#                            ['white', '#D2691E'],
+#                            ['white', 'blue'], ['white', 'blue'],
+#                            ['white', '#D2691E'], ['white', 'blue']]):
+#     ax.pcolormesh(df.data['time'], df.data['range'],
+#                   val.T, cmap=mpl.colors.ListedColormap(cmap_))
+# axes.flatten()[-1].pcolormesh(df.data['time'], df.data['range'],
+#                               df.data['classifier'].T,
+#                               cmap=cmap, norm=norm)
+# axes[0, 0].pcolormesh(df.data['time'], df.data['range'],
+#                       np.log10(df.data['beta_raw']).T,
+#                       cmap='jet', vmin=-8, vmax=-4)
+# axes[0, 1].pcolormesh(df.data['time'], df.data['range'],
+#                       df.data['v_raw'].T, cmap='jet', vmin=-2, vmax=2)
+# fig.tight_layout()
+# fig.savefig(classifier_folder + '/' + df.filename + '_classifier.png',
+#             dpi=150, bbox_inches='tight')
 
 # %%
-classifier = df.data['classifier'].flatten()
-result = pd.DataFrame({'date': df.date,
-                       'location': df.location,
-                       'beta': np.log10(beta_save),
-                       'v': v_save,
-                       'depo': depo_save,
-                       'co_signal': co_save,
-                       'cross_signal': cross_save,
-                       'time': time_save,
-                       'range': range_save,
-                       'classifier': classifier})
-
-result.to_csv(classifier_folder + '/' + df.filename + '_classified.csv',
-              index=False)
-
-# %%
-fig = plt.figure(figsize=(16, 9))
-gs = fig.add_gridspec(4, 2, width_ratios=[5, 4])
-ax1 = fig.add_subplot(gs[0, 0])
-ax2 = fig.add_subplot(gs[0, 1])
-ax3 = fig.add_subplot(gs[1, 0], sharex=ax1, sharey=ax1)
-ax4 = fig.add_subplot(gs[1, 1], sharex=ax2)
-ax5 = fig.add_subplot(gs[2, 0], sharex=ax1, sharey=ax1)
-ax6 = fig.add_subplot(gs[2, 1], sharex=ax2)
-ax7 = fig.add_subplot(gs[3, 0], sharex=ax1, sharey=ax1)
-ax8 = fig.add_subplot(gs[3, 1], sharex=ax2)
+fig, ax = plt.subplots(4, 1, figsize=(6, 8))
+ax1, ax3, ax5, ax7 = ax.ravel()
 p1 = ax1.pcolormesh(df.data['time'], df.data['range'],
                     np.log10(df.data['beta_raw']).T, cmap='jet', vmin=-8, vmax=-4)
 p2 = ax3.pcolormesh(df.data['time'], df.data['range'],
@@ -266,76 +236,24 @@ p4 = ax7.pcolormesh(df.data['time'], df.data['range'],
                     cmap=cmap, norm=norm)
 for ax in [ax1, ax3, ax5, ax7]:
     ax.yaxis.set_major_formatter(hd.m_km_ticks())
-    ax.set_ylabel('Height [km, a.g.l]')
+    ax.set_ylabel('Range [km, a.g.l]')
 
 cbar = fig.colorbar(p1, ax=ax1)
-cbar.ax.set_ylabel('Attenuated backscatter')
-cbar.ax.yaxis.set_label_position('left')
+cbar.ax.set_ylabel('Beta [' + df.units.get('beta_raw', None) + ']', rotation=90)
+# cbar.ax.yaxis.set_label_position('left')
 cbar = fig.colorbar(p2, ax=ax3)
-cbar.ax.set_ylabel('Velocity [m/s]')
-cbar.ax.yaxis.set_label_position('left')
+cbar.ax.set_ylabel('Velocity [' + df.units.get('v_raw', None) + ']', rotation=90)
+# cbar.ax.yaxis.set_label_position('left')
 cbar = fig.colorbar(p3, ax=ax5)
 cbar.ax.set_ylabel('Depolarization ratio')
-cbar.ax.yaxis.set_label_position('left')
-cbar = fig.colorbar(p4, ax=ax7, ticks=[5, 10.5, 15, 25, 35, 45])
-cbar.ax.set_yticklabels(['Background', 'Aerosol', 'Elevated aerosol',
+# cbar.ax.yaxis.set_label_position('left')
+cbar = fig.colorbar(p4, ax=ax7, ticks=[5, 15, 25, 35, 45])
+cbar.ax.set_yticklabels(['Background', 'Aerosol',
                          'Precipitation', 'Clouds', 'Undefined'])
-
-for i, ax, lab in zip([20, 30], [ax2, ax4],
-                      ['Precipitation', 'Cloud']):
-    ax.set_ylabel(lab)
-    if (classifier == i).any():
-        depo = depo_save[classifier == i]
-        depo = depo[depo < 0.8]
-        depo = depo[depo > -0.25]
-        ax.hist(depo, bins=40)
-
-bin_time = np.arange(0, 24+0.35, 0.25)
-bin_height = np.arange(0, df.data['range'].max() + 31, 30)
-
-mask_elevated_aerosol = classifier == 11
-if (mask_elevated_aerosol).any():
-    bin_time1h = np.arange(0, 24+0.5, 0.5)
-    co, _, _, _ = binned_statistic_2d(range_save[mask_elevated_aerosol],
-                                      time_save[mask_elevated_aerosol],
-                                      co_save[mask_elevated_aerosol],
-                                      bins=[bin_height, bin_time1h],
-                                      statistic=np.nanmean)
-    cross, _, _, _ = binned_statistic_2d(range_save[mask_elevated_aerosol],
-                                         time_save[mask_elevated_aerosol],
-                                         cross_save[mask_elevated_aerosol],
-                                         bins=[bin_height, bin_time1h],
-                                         statistic=np.nanmean)
-    depo = (cross-1)/(co-1)
-    depo = depo[depo < 0.8]
-    depo = depo[depo > -0.25]
-    ax6.hist(depo, bins=40)
-ax6.set_ylabel('Elevated_aerosol')
-ax8.set_ylabel('Aerosol')
-
-mask_aerosol = classifier == 10
-if (mask_aerosol).any():
-    bin_time1h = np.arange(0, 24+0.5, 0.5)
-    co, _, _, _ = binned_statistic_2d(range_save[mask_aerosol],
-                                      time_save[mask_aerosol],
-                                      co_save[mask_aerosol],
-                                      bins=[bin_height, bin_time1h],
-                                      statistic=np.nanmean)
-    cross, _, _, _ = binned_statistic_2d(range_save[mask_aerosol],
-                                         time_save[mask_aerosol],
-                                         cross_save[mask_aerosol],
-                                         bins=[bin_height, bin_time1h],
-                                         statistic=np.nanmean)
-    depo = (cross-1)/(co-1)
-    depo = depo[depo < 0.8]
-    depo = depo[depo > -0.25]
-    ax8.hist(depo, bins=40)
-
-ax8.set_xlabel('Depolarization ratio', weight='bold')
-ax7.set_xlabel('Time UTC [hour]', weight='bold')
+ax7.set_xlabel('Time [UTC - hour]')
 
 fig.tight_layout()
-fig.savefig(classifier_folder + '/' + df.filename + '_hist.png',
+fig.savefig(classifier_folder + '/' + df.filename + '_classified.png',
             dpi=150, bbox_inches='tight')
 
 
