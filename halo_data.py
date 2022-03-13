318356f66048dd91bd16b93b752960705ae9d894
vietle <viet.letranhoang@gmail.com>
2022-03-13 18:41:53 +0200

    add bleed_through function

--- halo_data.py
+++ halo_data.py
@@ -1034,3 +1034,50 @@ def aggregate_data(nc_path,
     result = pd.DataFrame({'depo': depo_list, 'v_raw': v_list,
                            'beta_raw': beta_list, 'date': date_list})
     return result, time_list, range_list
+
+
+def bleed_through(df):
+    # Correction for bleed through and remove all observations below 90m
+    with open('summary_info.json', 'r') as file:
+        summary_info = json.load(file)
+    df = df.where(df.range > 90, drop=True)
+    file_date = '-'.join([str(int(df.attrs[ele])).zfill(2) for
+                          ele in ['year', 'month', 'day']])
+    file_location = '-'.join([df.attrs['location'], str(int(df.attrs['systemID']))])
+    df.attrs['file_name'] = file_date + '-' + file_location
+
+    if '32' in file_location:
+        for period in summary_info['32']:
+            if (period['start_date'] <= file_date) & \
+                    (file_date <= period['end_date']):
+                df.attrs['background_snr_sd'] = period['snr_sd']
+                df.attrs['bleed_through_mean'] = period['bleed_through']['mean']
+                df.attrs['bleed_through_sd'] = period['bleed_through']['sd']
+    else:
+        id = str(int(df.attrs['systemID']))
+        df.attrs['background_snr_sd'] = summary_info[id]['snr_sd']
+        df.attrs['bleed_through_mean'] = summary_info[id]['bleed_through']['mean']
+        df.attrs['bleed_through_sd'] = summary_info[id]['bleed_through']['sd']
+    bleed = df.attrs['bleed_through_mean']
+    sigma_bleed = df.attrs['bleed_through_sd']
+    sigma_co, sigma_cross = df.attrs['background_snr_sd'], df.attrs['background_snr_sd']
+
+    df['cross_signal_bleed'] = (['time', 'range'], ((df['cross_signal'] - 1) -
+                                                    bleed * (df['co_signal'] - 1) + 1).data)
+
+    df['cross_signal_bleed_sd'] = np.sqrt(
+        sigma_cross**2 +
+        ((bleed * (df['co_signal'] - 1))**2 *
+         ((sigma_bleed/bleed)**2 +
+          (sigma_co/(df['co_signal'] - 1))**2))
+    )
+    df['depo_bleed'] = (df['cross_signal_bleed'] - 1) / \
+        (df['co_signal'] - 1)
+
+    df['depo_bleed_sd'] = np.sqrt(
+        (df['depo_bleed'])**2 *
+        (
+            (df['cross_signal_bleed_sd']/(df['cross_signal_bleed'] - 1))**2 +
+            (sigma_co/(df['co_signal']-1))**2
+        ))
+    return df
