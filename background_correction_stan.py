72fdaeed0cfafbb8eb2b9a88028ea97f05b40d75
viet_fmi-linux <viet.letranhoang@gmail.com>
2022-05-16 10:23:45 +0300

    stan in linux

--- background_correction_stan.py
+++ background_correction_stan.py
@@ -7,7 +7,7 @@ import pandas as pd
 import xarray as xr
 import pystan
 import pickle
-%matplotlib qt
+
 # %%
 
 
@@ -24,7 +24,6 @@ def standard_scale_reverse(a, b, c, x0, y0):
     a0 = meany + stdy*a - stdy/stdx*b*meanx + stdy*c/((stdx)**2)*(meanx**2)
     b0 = stdy/stdx*b - 2*meanx*stdy*c/((stdx)**2)
     c0 = stdy*c/((stdx)**2)
-    # e0 = stdy*e
     return a0, b0, c0
 
 
@@ -90,10 +89,12 @@ y[i] ~normal(mu[i], sigma);
 #     pickle.dump(model, f)
 
 # %%
-save_folder = r'F:\halo\paper\figures\background_correction_all/46/'
-file_list = glob.glob(r'F:\halo\classifier_new\46/*.nc')
-for file in [file_list[1]]:
-    file = r'F:\halo\classifier_new\46/2018-07-27-Hyytiala-46_classified.nc'
+model = pickle.load(open('background_correction_model.pkl', 'rb'))
+save_folder = r'/media/le/Elements/halo/paper/figures/background_correction_all/stan/54/'
+# file_list = glob.glob(r'/media/le/Elements/halo/classifier_new/32/*.nc')
+file_list = glob.glob(r'/media/le/Elements/54/classified/*.nc')
+file_list = sorted(file_list)
+for file in file_list:
     print(file)
     df = xr.open_dataset(file)
     df = hd.bleed_through(df)  # just to get the bleed through mean and std
@@ -111,15 +112,17 @@ for file in [file_list[1]]:
     for t in avg['time']:
         print(t.values)
         range_aerosol = avg['aerosol_percentage'].loc[t, :].values > 0.8
+        if np.sum(range_aerosol) == 0:
+            continue
         co_mean_profile = avg['co_signal'].loc[t, :].values
         cross_mean_profile = avg['cross_signal'].loc[t, :].values
         if all(np.isnan(co_mean_profile)) | all(np.isnan(cross_mean_profile)):
             continue
         background = hd.background_detection(co_mean_profile, threshold)
-        if np.sum(background)/len(background) < 0.5:
+        background_percent = np.sum(background)/len(background)
+        if background_percent < 0.1:
             continue
 
-        model = pickle.load(open('background_correction_model.pkl', 'rb'))
         print('loaded model')
         _, co_fitted, co_corrected = SNR_fit(co_mean_profile, background, model)
         _, cross_fitted, cross_corrected = SNR_fit(cross_mean_profile, background,
@@ -135,16 +138,16 @@ for file in [file_list[1]]:
         depo_corrected = (cross_corrected_bleed - 1) / (co_corrected - 1)
         depo = (cross_mean_profile - 1)/(co_mean_profile - 1)
 
-        fig, ax = plt.subplots(1, 4, figsize=(12, 6))
+        fig, ax = plt.subplots(1, 4, figsize=(16, 9))
         ax[0].scatter(cross_mean_profile, avg['range'],
                       alpha=0.3, label='cross_signal', c='blue', edgecolors='none', s=20)
-        ax[0].plot(np.mean(cross_fitted, axis=0), avg['range'], c='blue', label='wavelet-fit cross')
+        ax[0].plot(np.mean(cross_fitted, axis=0), avg['range'], c='blue', label='fitted cross')
         ax[0].plot(cross_mean_profile[background], avg['range'][background], 'x',
                    alpha=0.3, label='background_cross', c='blue')
 
         ax[0].scatter(co_mean_profile, avg['range'],
                       alpha=0.3, label='co_signal', c='red', edgecolors='none', s=20)
-        ax[0].plot(np.mean(co_fitted, axis=0), avg['range'], c='red', label='wavelet-fit co')
+        ax[0].plot(np.mean(co_fitted, axis=0), avg['range'], c='red', label='fitted co')
         ax[0].plot(co_mean_profile[background], avg['range'][background], 'x',
                    alpha=0.3, label='background_co', c='red')
         ax[0].set_xlim([0.9995, 1.003])
@@ -152,17 +155,17 @@ for file in [file_list[1]]:
 
         ax[1].errorbar(np.mean(cross_corrected, axis=0), avg['range'],
                        xerr=np.std(cross_corrected, axis=0), fmt='.',
-                       label='corrected cross', errorevery=1, elinewidth=0.5,
+                       errorevery=1, elinewidth=0.5,
                        c='blue', alpha=0.5)
-        ax[1].set_xlabel('SNR')
+        ax[1].set_xlabel('corrected cross-SNR')
         ax[1].set_xlim([0.9995, 1.003])
 
         ax[2].errorbar(np.mean(co_corrected, axis=0), avg['range'],
                        xerr=np.std(co_corrected, axis=0), fmt='.',
-                       label='corrected co', errorevery=1, elinewidth=0.5,
+                       errorevery=1, elinewidth=0.5,
                        color='red', alpha=0.5)
         ax[2].set_xlim([0.9995, 1.003])
-        ax[2].set_xlabel('SNR')
+        ax[2].set_xlabel('corrected co-SNR')
 
         ax[3].plot(depo[range_aerosol],
                    avg['range'][range_aerosol], '.',
@@ -173,25 +176,27 @@ for file in [file_list[1]]:
                        label='corrected depo',
                        errorevery=1, elinewidth=0.5, fmt='.')
         ax[3].set_xlim([-0.05, 0.4])
-        ax[3].set_xlabel('Depolarization ratio (only Aerosol)')
+        ax[3].set_xlabel(r'$\delta$ (only Aerosol)')
 
         for ax_ in ax.flatten():
             ax_.grid()
-            ax_.legend()
             ax_.yaxis.set_major_formatter(hd.m_km_ticks())
-            ax_.set_ylabel('Height [km]')
+        ax[0].legend(loc='upper right')
+        ax[3].legend()
+        ax[0].set_ylabel('Height [km]')
 
         result = pd.DataFrame.from_dict({
             'time': t.values,
             'range': avg['range'][range_aerosol].values,
-            # original depo value
             'depo': depo[range_aerosol],
             'depo_corrected': np.mean(depo_corrected, axis=0)[range_aerosol],
             'depo_corrected_sd': np.std(depo_corrected, axis=0)[range_aerosol],
             'co_corrected': np.mean(co_corrected, axis=0)[range_aerosol],
             'co_corrected_sd': np.std(co_corrected, axis=0)[range_aerosol],
             'cross_corrected': np.mean(cross_corrected_bleed, axis=0)[range_aerosol],
-            'cross_corrected_sd': np.std(cross_corrected_bleed, axis=0)[range_aerosol]
+            'cross_corrected_sd': np.std(cross_corrected_bleed, axis=0)[range_aerosol],
+            'background_percent': background_percent
+
         })
         depo_sub_folder = save_folder + '/' + df.attrs['file_name']
         Path(depo_sub_folder).mkdir(parents=True, exist_ok=True)
