9923e9cc4d029bba1e192bf6d47cb9079eccd177
viet_fmi-linux <viet.letranhoang@gmail.com>
2022-05-28 19:40:13 +0300

    focus function with stan

--- focus_function.py
+++ focus_function.py
@@ -1,9 +1,9 @@
 import json
 from sklearn.metrics import r2_score
-import dask.array as da
 import matplotlib.cm as cm
 import calendar
 from scipy.stats import binned_statistic_2d
+from scipy.stats import binned_statistic
 import os
 from scipy.ndimage import uniform_filter
 from sklearn.cluster import DBSCAN
@@ -15,7 +15,6 @@ import copy
 from matplotlib.patches import Patch
 from matplotlib.lines import Line2D
 import datetime
-import statsmodels.api as sm
 from sklearn.linear_model import LinearRegression
 from scipy.optimize import curve_fit
 from sklearn.mixture import GaussianMixture
@@ -36,29 +35,34 @@ import scipy.stats as stats
 from netCDF4 import Dataset
 import pywt
 import itertools
-%matplotlib qt
-
+import pystan
+import pickle
 # %%
 #######################################
 
 
 # %%
-df = xr.open_dataset(r'F:\halo\classifier_new\32/2019-04-05-Uto-32_classified.nc')
+save_dir = '/media/le/Elements/halo/paper/figures/focus_function/'
+# df = xr.open_dataset(r'F:\halo\classifier_new\32/2019-04-05-Uto-32_classified.nc')
+df = xr.open_dataset(r'/media/le/Elements/halo/classifier_new/32/2019-04-05-Uto-32_classified.nc')
 avg = df[['beta_raw', 'co_signal', 'cross_signal']].resample(time='60min').mean(dim='time')
 
-ceilo = Dataset(r'F:\halo\paper\uto_ceilo_20190405\20190405_uto_cl31.nc')
+# ceilo = Dataset(r'F:\halo\paper\uto_ceilo_20190405\20190405_uto_cl31.nc')
+ceilo = Dataset(r'/media/le/Elements/halo/paper/uto_ceilo_20190405/20190405_uto_cl31.nc')
 ceilo_beta = ceilo['beta_raw'][:]
-ceilo_time = ceilo['time'][:]
 ceilo_range = ceilo['range'][:]
-
-ceilo_profile = np.nanmean(ceilo_beta[np.floor(ceilo_time) == 2], axis=0)
+ceilo_time = [pd.Timedelta(x, 'H') + pd.to_datetime(df.time.values[0]).floor('D')
+              for x in ceilo['time'][:]]
+ceilo_time = pd.to_datetime(ceilo_time)
+ceilo_time_seconds = ceilo_time.hour*3600+ceilo_time.minute*60+ceilo_time.second
+halo_time = pd.to_datetime(df.time.values)
+halo_time_seconds = halo_time.hour*3600+halo_time.minute*60+halo_time.second
+ceilo_profile = np.nanmean(ceilo_beta[ceilo_time.hour < 2], axis=0)
 
 # %%
 
 
-def beta(SNR, R, f=625, D=12e-3):
-    # f = 625
-    # D = 12e-3
+def beta(SNR, R, f=308, D=18e-3):
     wavelength = df['wavelength'].values[0]  # 1.5e-6 m
     c = 3e8
     h = 6.626e-34
@@ -72,7 +76,140 @@ def beta(SNR, R, f=625, D=12e-3):
 
     return beta
 
+#
+# # %%
+# fig, ax = plt.subplots(3, 3, sharey=True, sharex=True, figsize=(12, 6))
+#
+# for ax_, (f, D) in zip(ax.flatten(), list(itertools.product([400, 500, 600], [12e-3, 17e-3, 22e-3]))):
+#     ax_.plot(np.log10(beta(avg['co_signal'][0, :]-1,
+#                            df['range'], f, D)), df['range']/1000, '.')
+#     ax_.set_title(f'f:{f}m, D:{D}m')
+#
+#
+# # ax[0].set_ylim([0, 3])
+# for ax_ in ax.flatten():
+#     ax_.grid()
+#     ax_.set_xlim([-8, -4])
 
+# %%
+
+
+df['co_signal'] = xr.where(
+    df['co_signal'] < 3*df.attrs['background_snr_sd'] + 1, np.nan, df['co_signal'])
+halo_range = df['range'].values
+halo_range = halo_range[halo_range > 500]
+final_range_bin = np.append((halo_range - 15), (halo_range + 15)[-1])
+final_range_bin = final_range_bin[final_range_bin < 7710]
+halo_snr = (df['co_signal']-1)[:, df['range'] > 500]
+final_time_bin = pd.date_range('2019-04-05', periods=34, freq='30min')
+final_time_bin_seconds = final_time_bin.hour*3600 + final_time_bin.minute*60 + final_time_bin.minute
+
+# %%
+ceilo_beta_flattened = ceilo_beta.flatten()
+ceilo_time_seconds_flattened = np.repeat(ceilo_time_seconds, ceilo_range.size)
+ceilo_range_flattened = np.tile(ceilo_range*1000, ceilo_time_seconds.size)
+ceilo_beta_binned, _, _, _ = binned_statistic_2d(ceilo_time_seconds_flattened,
+                                                 ceilo_range_flattened, ceilo_beta_flattened,
+                                                 bins=[final_time_bin_seconds, final_range_bin],
+                                                 statistic=np.nanmean)
+ceilo_beta_count, _, _, _ = binned_statistic_2d(ceilo_time_seconds_flattened,
+                                                ceilo_range_flattened, ceilo_beta_flattened,
+                                                bins=[final_time_bin_seconds, final_range_bin],
+                                                statistic='count')
+
+# %%
+halo_snr_flattened = halo_snr.values.flatten()
+halo_time_seconds_flattened = np.repeat(halo_time_seconds, halo_range.size)
+halo_range_flattened = np.tile(halo_range, halo_time_seconds.size)
+halo_snr_binned, _, _, _ = binned_statistic_2d(halo_time_seconds_flattened,
+                                               halo_range_flattened, halo_snr_flattened,
+                                               bins=[final_time_bin_seconds, final_range_bin],
+                                               statistic=np.nanmean)
+halo_snr_count, _, _, _ = binned_statistic_2d(halo_time_seconds_flattened,
+                                              halo_range_flattened, halo_snr_flattened,
+                                              bins=[final_time_bin_seconds, final_range_bin],
+                                              statistic='count')
+
+# %%
+halo_snr_binned[halo_snr_count < 5] = np.nan
+
+# %%
+data = pd.DataFrame({'halo_snr': halo_snr_binned.flatten(),
+                     'ceilo_beta': ceilo_beta_binned.flatten(),
+                     'time': np.repeat(final_time_bin[:-1], final_range_bin.size-1),
+                     'range': np.tile(final_range_bin[:-1], final_time_bin.size - 1)})
+
+# %%
+data['ceilo_beta'] = np.log10(data['ceilo_beta'])
+data = data[data['ceilo_beta'] > -7.5]
+data = data[data['halo_snr'] > 0]
+data = data[data['range'] < 2000]
+data = data[data['range'] > 200]
+data = data.dropna(axis=0)
+data = data.reset_index(drop=True)
+
+#################################################
+# Stan model
+# # %%
+# focus_function = """
+# data{
+#     int < lower = 0 > N; // number of background observations
+#     real ceilo_beta[N]; // ceilometer's beta
+#     real lidar_snr[N]; // lidar's snr
+#     real range[N];
+# }
+# parameters{
+# real <lower = 0, upper = 10000>f;
+# real <lower = 0, upper = 0.060>D;
+# real <lower = 0> sigma2;
+# }
+# transformed parameters{
+# real A[N];
+# real T;
+# real <lower=0> sigma;
+# real mu[N];
+# sigma = sqrt(sigma2);
+#
+# for(i in 1: N){
+# A[i] = pi()*D*D / (4*(1+(pi()*(D^2)/(4*1.5*10^(-6)*range[i]))^2*(1-range[i]/f)^2));
+# T = A[i]/((range[i])^2);
+# mu[i] = log10((2*6.626*10^(-34)*(3*10^8/(1.5*10^(-6)))*50000000)/(1*3*10^2*9.9999) * (lidar_snr[i]/T));
+# }}
+# model{
+# f ~ normal(0, sqrt(1e3));
+# D ~ normal(0, sqrt(1e1));
+# sigma2 ~inv_gamma(0.001, 0.001);
+#
+# for(i in 1: N)
+# ceilo_beta[i] ~normal(mu[i], sigma);
+# }
+# """
+#
+# # %%
+# model = pystan.StanModel(model_code=focus_function)
+# with open('focus_function_model.pkl', 'wb') as f:
+#     pickle.dump(model, f)
+
+############################################
+# %%
+model = pickle.load(open('focus_function_model.pkl', 'rb'))
+dat = dict(N=data.shape[0], ceilo_beta=data['ceilo_beta'],
+           lidar_snr=data['halo_snr'], range=data['range'])
+fit = model.sampling(data=dat, warmup=500, iter=2000, chains=4, thin=1)
+
+# %%
+f = np.mean(fit['f'])
+D = np.mean(fit['D'])
+fig, ax = plt.subplots(1, 3, figsize=(12, 9))
+ax[0].hist(fit['f'], bins=20)
+ax[0].set_title(f"f: {f:.3f}")
+ax[1].hist(fit['D'], bins=20)
+ax[1].set_title(f"D: {D:.3f}")
+ax[2].hist(fit['sigma'], bins=20)
+ax[2].set_title(f"sigma: {np.mean(fit['sigma']):.3f}")
+fig.savefig(save_dir + f"stan_result_{f:.0f}_{1000*(D):.0f}", bbox_inches='tight')
+
+# %%
 fig, ax = plt.subplots(1, 3, sharey=True, sharex=True, figsize=(12, 6))
 
 ax[0].plot(np.log10(ceilo_profile), ceilo['range'][:], '.')
@@ -81,56 +218,26 @@ ax[0].set_title(r'$\beta$ ceilometer')
 ax[1].plot(np.log10(avg['beta_raw'][0, :]), df['range']/1000, '.')
 ax[1].set_title(r'$\beta$ XR')
 
-# ax[2].plot(avg['co_signal'][0, :], df['range']/1000, '.')
-# ax[2].set_title(r'co_signal XR')
-#
-# ax[3].plot(avg['cross_signal'][0, :], df['range']/1000, '.')
-# ax[3].set_title(r'cross_signal XR')
-
 ax[2].plot(np.log10(beta(avg['co_signal'][0, :]-1,
-                         df['range'])), df['range']/1000, '.')
-ax[2].set_title(r'$\beta$ XR - corrected')
+                         df['range'], f=f, D=D)), df['range']/1000, '.')
+ax[2].set_title(f'f: {f:.3f}, D: {D:.3f}')
 
 # ax[0].set_ylim([0, 3])
 for ax_ in ax.flatten():
     ax_.grid()
 
 ax[0].set_ylabel('Height [km]')
+fig.savefig(save_dir + f"first_profile_{f:.0f}_{1000*(D):.0f}", bbox_inches='tight')
 
 # %%
-fig, ax = plt.subplots(3, 3, sharey=True, sharex=True, figsize=(12, 6))
-
-for ax_, (f, D) in zip(ax.flatten(), list(itertools.product([400, 500, 600], [12e-3, 17e-3, 22e-3]))):
-    ax_.plot(np.log10(beta(avg['co_signal'][0, :]-1,
-                           df['range'], f, D)), df['range']/1000, '.')
-    ax_.set_title(f'f:{f}m, D:{D}m')
-
-
-# ax[0].set_ylim([0, 3])
-for ax_ in ax.flatten():
-    ax_.grid()
-    ax_.set_xlim([-8, -4])
-
-
-# %%
-list(itertools.product([400, 500, 600], [12, 17, 22]))
-
-# %%
-avg['co_signal'].shape
-beta_corrected = np.array([beta(df['co_signal'][x, :].values-1,
-                                df['range'].values) for x in range(df['co_signal'].shape[0])])
-
-# %%
-beta_corrected.shape
-
-# %%
-
-fig, ax = plt.subplots(3, 1, sharex=True, sharey=True)
-p0 = ax[0].pcolormesh(df.time, df.range, np.log10(beta_corrected).T, cmap='jet',
+fig, ax = plt.subplots(3, 1, sharex=True, sharey=True, figsize=(12, 9))
+p0 = ax[0].pcolormesh(df.time, df.range,
+                      np.log10(beta(df['co_signal']-1,
+                                    df['range'], f=f, D=D)).T, cmap='jet',
                       vmin=-8, vmax=-4)
 p1 = ax[1].pcolormesh(df.time, df.range, np.log10(df.beta_raw).T, cmap='jet',
                       vmin=-8, vmax=-4)
-p2 = ax[2].pcolormesh(ceilo_time, ceilo['range'][:]*1000, np.log10(ceilo_beta).T, cmap='jet',
+p2 = ax[2].pcolormesh(ceilo_time, ceilo_range*1000, np.log10(ceilo_beta).T, cmap='jet',
                       vmin=-8, vmax=-4)
 
 for ax_, p, lab in zip(ax.flatten(), [p0, p1, p2],
@@ -142,4 +249,20 @@ for ax_, p, lab in zip(ax.flatten(), [p0, p1, p2],
     cbar.ax.set_ylabel(lab, rotation=90)
     cbar.ax.set_title(r'$1e$', size=10)
     ax_.set_ylabel('Height a.g.l [km]')
+
+fig.savefig(save_dir + f"over_view_{f:.0f}_{1000*(D):.0f}", bbox_inches='tight')
+
 # %%
+f_D = pd.DataFrame({'f': fit['f'].flatten(), 'D': fit['D'].flatten()})
+H, f_edges, D_edges = np.histogram2d(
+    f_D['f'],
+    f_D['D'],
+    bins=100)
+X, Y = np.meshgrid(f_edges, D_edges)
+fig, ax = plt.subplots(figsize=(9, 6))
+p = ax.pcolormesh(X, Y, H.T, norm=LogNorm())
+ax.set_xlabel('f')
+ax.set_ylabel('D')
+colorbar = fig.colorbar(p, ax=ax)
+colorbar.ax.set_ylabel('N')
+fig.savefig(save_dir + f"f_D_{f:.0f}_{1000*(D):.0f}", bbox_inches='tight')
