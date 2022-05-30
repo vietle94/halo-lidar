c9fb5b23a0f55333cb438b6150d912e138b7ee76
viet_fmi-linux <viet.letranhoang@gmail.com>
2022-05-30 21:39:48 +0300

    focus function in Linux

--- focus_function.py
+++ focus_function.py
@@ -42,14 +42,14 @@ import pickle
 # %%
 #######################################
 
-# save_dir = '/media/le/Elements/halo/paper/figures/focus_function/'
-save_dir = r'F:\halo/paper/figures/focus_function/'
-df = xr.open_dataset(r'F:\halo\classifier_new\32/2019-04-05-Uto-32_classified.nc')
-# df = xr.open_dataset(r'/media/le/Elements/halo/classifier_new/32/2019-04-05-Uto-32_classified.nc')
+save_dir = '/media/le/Elements/halo/paper/figures/focus_function/'
+# save_dir = r'F:\halo/paper/figures/focus_function/'
+# df = xr.open_dataset(r'F:\halo\classifier_new\32/2019-04-05-Uto-32_classified.nc')
+df = xr.open_dataset(r'/media/le/Elements/halo/classifier_new/32/2019-04-05-Uto-32_classified.nc')
 avg = df[['beta_raw', 'co_signal', 'cross_signal']].resample(time='60min').mean(dim='time')
 
-ceilo = Dataset(r'F:\halo\paper\uto_ceilo_20190405\20190405_uto_cl31.nc')
-# ceilo = Dataset(r'/media/le/Elements/halo/paper/uto_ceilo_20190405/20190405_uto_cl31.nc')
+# ceilo = Dataset(r'F:\halo\paper\uto_ceilo_20190405\20190405_uto_cl31.nc')
+ceilo = Dataset(r'/media/le/Elements/halo/paper/uto_ceilo_20190405/20190405_uto_cl31.nc')
 ceilo_beta = ceilo['beta_raw'][:]
 ceilo_range = ceilo['range'][:]
 ceilo_time = [pd.Timedelta(x, 'H') + pd.to_datetime(df.time.values[0]).floor('D')
@@ -103,7 +103,7 @@ def beta(SNR, R, f=308, D=18e-3):
 df['co_signal'] = xr.where(
     df['co_signal'] < 3*df.attrs['background_snr_sd'] + 1, np.nan, df['co_signal'])
 halo_range = df['range'].values
-min_range = 200
+min_range = 200 + 16
 halo_range = halo_range[halo_range > min_range]
 final_range_bin = np.append((halo_range - 15), (halo_range + 15)[-1])
 final_range_bin = final_range_bin[final_range_bin < 7710]
@@ -140,96 +140,96 @@ data = pd.DataFrame({'halo_snr': halo_snr_binned.flatten(),
 data['ceilo_beta'] = np.log10(data['ceilo_beta'])
 data = data.dropna(axis=0)
 data = data.reset_index(drop=True)
-data = data[data['range'] < 1500]
+data = data[data['range'] < 1900]
 
 # %%
-f = np.linspace(100, 1000, 100)
-D = np.linspace(10e-3, 60e-3, 100)
-
-result_rms = []
-result_f = []
-result_D = []
-for grp_name, grp_value in data.groupby(data['time']):
-    rms_base = 1100
-    f_base = 900
-    D_base = 900
-    for fi, f_ in enumerate(f):
-        for Di, D_ in enumerate(D):
-            corrected_beta = beta(grp_value['halo_snr'], grp_value['range'], f=f_, D=D_)
-            corrected_beta = np.log10(corrected_beta)
-            new_rms = np.sqrt(np.mean((data['ceilo_beta'] - corrected_beta)**2))
-            if new_rms < rms_base:
-                rms_base = new_rms
-                f_base = f_
-                D_base = D_
-    result_rms.append(rms_base)
-    result_f.append(f_base)
-    result_D.append(D_base)
-
-# %%
-%matplotlib qt
-fig, ax = plt.subplots(figsize=(12, 9))
-H, x_edges, y_edges = np.histogram2d(
-    result_f,
-    result_D,
-    bins=[np.linspace(0, 1000, 120), np.linspace(0, 60e-3, 120)])
-
-H[H == 0] = np.nan
-X, Y = np.meshgrid(x_edges, y_edges)
-p = ax.pcolormesh(x_edges, y_edges, H.T)
-ax.set_xlim([000, 1000])
-ax.set_ylim([0e-3, 60e-3])
-ax.set_xlabel('f')
-ax.set_ylabel('D')
-cbar = fig.colorbar(p, ax=ax)
-cbar.ax.set_ylabel('N')
-fig.savefig(save_dir + '2d_histogram_f_D', bbox_inches='tight')
+# f = np.linspace(100, 1000, 100)
+# D = np.linspace(10e-3, 60e-3, 100)
+#
+# result_rms = []
+# result_f = []
+# result_D = []
+# for grp_name, grp_value in data.groupby(data['time']):
+#     rms_base = 1100
+#     f_base = 900
+#     D_base = 900
+#     for fi, f_ in enumerate(f):
+#         for Di, D_ in enumerate(D):
+#             corrected_beta = beta(grp_value['halo_snr'], grp_value['range'], f=f_, D=D_)
+#             corrected_beta = np.log10(corrected_beta)
+#             new_rms = np.sqrt(np.mean((data['ceilo_beta'] - corrected_beta)**2))
+#             if new_rms < rms_base:
+#                 rms_base = new_rms
+#                 f_base = f_
+#                 D_base = D_
+#     result_rms.append(rms_base)
+#     result_f.append(f_base)
+#     result_D.append(D_base)
 
 # %%
-scaler = StandardScaler()
-r2_matrix = np.empty((f.size, D.size))
-rms_matrix = np.empty((f.size, D.size))
-
-for fi, f_ in enumerate(f):
-    for Di, D_ in enumerate(D):
-        corrected_beta = beta(data['halo_snr'], data['range'], f=f_, D=D_)
-        corrected_beta = np.log10(corrected_beta)
-        corrected_beta_compare = corrected_beta
-        ceilo_beta_compare = data['ceilo_beta']
-        r2_matrix[fi, Di] = pearsonr(ceilo_beta_compare, corrected_beta_compare)[0]
-        rms_matrix[fi, Di] = np.sqrt(np.mean((corrected_beta_compare - ceilo_beta_compare)**2))
-
-
-# %%
-fig, ax = plt.subplots(2, 1, figsize=(12, 6), sharex=True, sharey=True)
-p = ax[0].pcolormesh(f, D, r2_matrix.T)
-fig.colorbar(p, ax=ax[0])
-ax[0].set_title('Pearson_R')
-
-pp = ax[1].pcolormesh(f, D, rms_matrix.T)
-fig.colorbar(pp, ax=ax[1])
-ax[1].set_title('root_mean_squared_error')
+# %matplotlib qt
+# fig, ax = plt.subplots(figsize=(12, 9))
+# H, x_edges, y_edges = np.histogram2d(
+#     result_f,
+#     result_D,
+#     bins=[np.linspace(0, 1000, 120), np.linspace(0, 60e-3, 120)])
+#
+# H[H == 0] = np.nan
+# X, Y = np.meshgrid(x_edges, y_edges)
+# p = ax.pcolormesh(x_edges, y_edges, H.T)
+# ax.set_xlim([000, 1000])
+# ax.set_ylim([0e-3, 60e-3])
+# ax.set_xlabel('f')
+# ax.set_ylabel('D')
+# cbar = fig.colorbar(p, ax=ax)
+# cbar.ax.set_ylabel('N')
+# fig.savefig(save_dir + '2d_histogram_f_D', bbox_inches='tight')
 
 # %%
-fig, ax = plt.subplots(3, 3, sharey=True, sharex=True, figsize=(12, 6))
-
-for ax_, (f, D) in zip(ax.flatten(), list(itertools.product([100, 300, 900], [10e-3, 18e-3, 30e-3]))):
-    mask = data['range'] < 1100
-    x_data = np.log10(beta(data['halo_snr'][mask], data['range'][mask], f, D))
-    y_data = data['ceilo_beta'][mask]
-    ax_.plot(x_data, y_data, '.', alpha=0.1)
-    ax_.set_title(f'f:{f}m, D:{D}m')
-
-    axline_min = np.nanmin((np.nanmin(x_data), np.nanmin(y_data)))
-    axline_max = np.nanmin((np.nanmax(x_data), np.nanmax(y_data)))
-    ax_.axline((axline_min, axline_min),
-               (axline_max, axline_max),
-               color='grey', linewidth=0.5, ls='--')
-
-    ax_.grid()
-    ax_.set_xlim([-8, -4])
-    ax_.set_ylim([-8, -4])
-    ax_.set_xticks([-8, -7, -6, -5, -4])
+# scaler = StandardScaler()
+# r2_matrix = np.empty((f.size, D.size))
+# rms_matrix = np.empty((f.size, D.size))
+#
+# for fi, f_ in enumerate(f):
+#     for Di, D_ in enumerate(D):
+#         corrected_beta = beta(data['halo_snr'], data['range'], f=f_, D=D_)
+#         corrected_beta = np.log10(corrected_beta)
+#         corrected_beta_compare = corrected_beta
+#         ceilo_beta_compare = data['ceilo_beta']
+#         r2_matrix[fi, Di] = pearsonr(ceilo_beta_compare, corrected_beta_compare)[0]
+#         rms_matrix[fi, Di] = np.sqrt(np.mean((corrected_beta_compare - ceilo_beta_compare)**2))
+#
+#
+# # %%
+# fig, ax = plt.subplots(2, 1, figsize=(12, 6), sharex=True, sharey=True)
+# p = ax[0].pcolormesh(f, D, r2_matrix.T)
+# fig.colorbar(p, ax=ax[0])
+# ax[0].set_title('Pearson_R')
+#
+# pp = ax[1].pcolormesh(f, D, rms_matrix.T)
+# fig.colorbar(pp, ax=ax[1])
+# ax[1].set_title('root_mean_squared_error')
+#
+# # %%
+# fig, ax = plt.subplots(3, 3, sharey=True, sharex=True, figsize=(12, 6))
+#
+# for ax_, (f, D) in zip(ax.flatten(), list(itertools.product([100, 300, 900], [10e-3, 18e-3, 30e-3]))):
+#     mask = data['range'] < 1100
+#     x_data = np.log10(beta(data['halo_snr'][mask], data['range'][mask], f, D))
+#     y_data = data['ceilo_beta'][mask]
+#     ax_.plot(x_data, y_data, '.', alpha=0.1)
+#     ax_.set_title(f'f:{f}m, D:{D}m')
+#
+#     axline_min = np.nanmin((np.nanmin(x_data), np.nanmin(y_data)))
+#     axline_max = np.nanmin((np.nanmax(x_data), np.nanmax(y_data)))
+#     ax_.axline((axline_min, axline_min),
+#                (axline_max, axline_max),
+#                color='grey', linewidth=0.5, ls='--')
+#
+#     ax_.grid()
+#     ax_.set_xlim([-8, -4])
+#     ax_.set_ylim([-8, -4])
+#     ax_.set_xticks([-8, -7, -6, -5, -4])
 
 # %%
 #################################################
@@ -290,7 +290,7 @@ fig, ax = plt.subplots(1, 3, figsize=(12, 9))
 ax[0].hist(fit['f'], bins=20)
 ax[0].set_title(f"f: {f:.3f} \n {f_sd:.3f}")
 ax[1].hist(fit['D'], bins=20)
-ax[1].set_title(f"D: {D:.3f} \n {D_sd:.6f}")
+ax[1].set_title(f"D: {D:.6f} \n {D_sd:.6f}")
 ax[2].hist(fit['sigma'], bins=20)
 ax[2].set_title(f"sigma: {np.mean(fit['sigma']):.3f}")
 fig.savefig(save_dir + f"stan_result_{f:.0f}_{1000*(D):.0f}", bbox_inches='tight')
